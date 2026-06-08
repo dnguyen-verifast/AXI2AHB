71fa312d1af789e7528625175b527c94861ef808
dnguyen-verifast <dnguyen@verifasttech.com>
2026-06-08 09:37:46 +0700

    ssss

--- testbench/top.sv
+++ testbench/top.sv
@@ -8,7 +8,7 @@ module top;
   import x2h_test_pkg::*;
 
   parameter  ADDR_WIDTHS = 32;
-  parameter DATA_WIDTHS = 64;
+  parameter DATA_WIDTHS = 32;
   logic aclk;
   logic aresetn;
   logic hclk;
@@ -28,62 +28,73 @@ module top;
     .resetn(hresetn)
   );
 
-  axi2ahb_bridge_top dut (
-    .aclk(aclk),
-    .aresetn(aresetn),
-    .hclk(hclk),
-    .hresetn(hresetn),
+axi_ahb_bridge_top #(
+    .DATA_WIDTH (32),
+    .ADDR_WIDTH (32),
+    .ID_WIDTH   (4)
+) dut (
 
-    .awid(axi_vif.awid),
-    .awaddr(axi_vif.awaddr),
-    .awlen(axi_vif.awlen),
-    .awsize(axi_vif.awsize),
-    .awburst(axi_vif.awburst),
-    .awvalid(axi_vif.awvalid),
-    .awready(axi_vif.awready),
+    //================ AXI =================
+    .axi_clk        (aclk),
+    .axi_rstn       (aresetn),
 
-    .wid(axi_vif.awid), 
-    .wdata(axi_vif.wdata),
-    .wstrb(axi_vif.wstrb),
-    .wlast(axi_vif.wlast),
-    .wvalid(axi_vif.wvalid),
-    .wready(axi_vif.wready),
+    .s_axi_awid     (axi_vif.awid),
+    .s_axi_awaddr   (axi_vif.awaddr),
+    .s_axi_awlen    (axi_vif.awlen),
+    .s_axi_awsize   (axi_vif.awsize),
+    .s_axi_awburst  (axi_vif.awburst),
+    .s_axi_awlock   (axi_vif.awlock[0]),
+    .s_axi_awcache  ({2'b00,axi_vif.awcache}),
+    .s_axi_awprot   (axi_vif.awprot),
+    .s_axi_awvalid  (axi_vif.awvalid),
+    .s_axi_awready  (axi_vif.awready),
 
-    .arid(axi_vif.arid),
-    .araddr(axi_vif.araddr),
-    .arlen(axi_vif.arlen),
-    .arsize(axi_vif.arsize),
-    .arburst(axi_vif.arburst),
-    .arvalid(axi_vif.arvalid),
-    .arready(axi_vif.arready),
+    .s_axi_wdata    (axi_vif.wdata),
+    .s_axi_wstrb    (axi_vif.wstrb),
+    .s_axi_wlast    (axi_vif.wlast),
+    .s_axi_wvalid   (axi_vif.wvalid),
+    .s_axi_wready   (axi_vif.wready),
 
-    .bid(axi_vif.bid),
-    .bresp(axi_vif.bresp),
-    .bvalid(axi_vif.bvalid),
-    .bready(axi_vif.bready),
+    .s_axi_bid      (axi_vif.bid),
+    .s_axi_bresp    (axi_vif.bresp),
+    .s_axi_bvalid   (axi_vif.bvalid),
+    .s_axi_bready   (axi_vif.bready),
 
-    .rid(axi_vif.rid),
-    .rdata(axi_vif.rdata),
-    .rresp(axi_vif.rresp),
-    .rlast(axi_vif.rlast),
-    .rvalid(axi_vif.rvalid),
-    .rready(axi_vif.rready),
+    .s_axi_arid     (axi_vif.arid),
+    .s_axi_araddr   (axi_vif.araddr),
+    .s_axi_arlen    (axi_vif.arlen),
+    .s_axi_arsize   (axi_vif.arsize),
+    .s_axi_arburst  (axi_vif.arburst),
+    .s_axi_arlock   (axi_vif.arlock[0]),
+    .s_axi_arcache  ({2'b00,axi_vif.arcache}),
+    .s_axi_arprot   (axi_vif.arprot),
+    .s_axi_arvalid  (axi_vif.arvalid),
+    .s_axi_arready  (axi_vif.arready),
 
-    .haddr(ahb_vif.haddr),
-    .htrans(ahb_vif.htrans),
-    .hwrite(ahb_vif.hwrite),
-    .hsize(ahb_vif.hsize),
-    .hburst(ahb_vif.hburst),
-    .hwdata(ahb_vif.hwdata),
-    .hbusreq(hbusreq_dummy),
-    .hlock(ahb_vif.hmastlock),
+    .s_axi_rid      (axi_vif.rid),
+    .s_axi_rdata    (axi_vif.rdata),
+    .s_axi_rresp    (axi_vif.rresp),
+    .s_axi_rlast    (axi_vif.rlast),
+    .s_axi_rvalid   (axi_vif.rvalid),
+    .s_axi_rready   (axi_vif.rready),
 
-    .hrdata(ahb_vif.hrdata),
-    .hready(ahb_vif.hready),
-    .hresp(ahb_vif.hresp),
-    .hgrant(hgrant_dummy),
-    .hmaster(ahb_vif.hmaster)
-  );
+    //================ AHB =================
+    .ahb_clk        (hclk),
+    .ahb_rstn       (hresetn),
+
+    .m_ahb_haddr    (ahb_vif.haddr),
+    .m_ahb_hburst   (ahb_vif.hburst),
+    .m_ahb_hsize    (ahb_vif.hsize),
+    .m_ahb_htrans   (ahb_vif.htrans),
+    .m_ahb_hwdata   (ahb_vif.hwdata),
+    .m_ahb_hwrite   (ahb_vif.hwrite),
+    .m_ahb_hsel     (ahb_vif.hsel),
+    .m_ahb_hmastlock(ahb_vif.hmastlock),
+
+    .m_ahb_hrdata   (ahb_vif.hrdata),
+    .m_ahb_hready   (ahb_vif.hready),
+    .m_ahb_hresp    (ahb_vif.hresp)
+);
 
   initial begin
     aclk = 0;
